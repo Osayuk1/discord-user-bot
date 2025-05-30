43b8797f37053e1aef2163eee64445db1fee7277
Osayuk1 <157780677+Osayuk1@users.noreply.github.com>
2025-05-30 22:27:14 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,7 @@
 from discord.ext import commands
-from discord import Intents
 import os
-import importlib.util
 import glob
+import importlib.util
 from keep_alive import keep_alive
 
 token = os.getenv("TOKEN")
@@ -13,12 +12,12 @@ bot = commands.Bot(command_prefix="!", self_bot=True)
 async def on_ready():
     print(f"{bot.user} is online!")
 
-# Load cogs from commands/
+# Load all commands from the commands folder
 for filename in glob.glob("commands/*.py"):
     spec = importlib.util.spec_from_file_location("module.name", filename)
-    foo = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(foo)
-    foo.setup(bot)
+    mod = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(mod)
+    mod.setup(bot)
 
 keep_alive()
 bot.run(token)
